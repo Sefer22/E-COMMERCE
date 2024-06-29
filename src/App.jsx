c64be3f18184a6aeaa37155f73424c38b6b4bbdb
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-29 23:52:14 +0400

    developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import PageContainer from './container/PageContainer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-
+      <PageContainer>
+        <Header />
+      </PageContainer>
     </div>
   )
 }
---
+++ src/container/PageContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function PageContainer(children) {
+    return
+    (
+        <div>{children}</div>
+    )
+}
+
+
+export default PageContainer
