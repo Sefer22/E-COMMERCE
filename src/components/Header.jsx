db33e4f74d611cc7f0c9a20fc708fc97a9225421
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-30 01:11:24 +0400

    developed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import '../css/Header.css';
 
 function Header() {
     return (
-        <div>Header</div>
+        <div>
+            <div>
+                <img className="logo" src="./src/images/logo.png" />
+            </div>
+        </div>
     )
 }
 
