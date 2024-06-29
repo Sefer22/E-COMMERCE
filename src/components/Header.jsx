947481db3cabca48192942313d0f3b651d947978
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-30 03:14:46 +0400

    developed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,14 @@ import '../css/Header.css';
 
 function Header() {
     return (
-        <div>
-            <div>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <div className='flex-row'>
                 <img className="logo" src="./src/images/logo.png" />
                 <p className='logo-text'>Safar C.</p>
+            </div>
+
+            <div>
+                <input type="text" />
             </div>
         </div>
     )
