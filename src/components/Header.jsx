907e119c5a42fa01c62fbff746497fbc869c91ba
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-29 23:52:34 +0400

    developed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Container from '@mui/material/Container';
+
 
 function Header() {
     return (
--- src/container/PageContainer.jsx
+++ src/container/PageContainer.jsx
@@ -1,10 +1,15 @@
+import { Container } from '@mui/material'
 import React from 'react'
+import Container from '@mui/material/Container';
+
 
 
 function PageContainer(children) {
     return
     (
-        <div>{children}</div>
+        <Container>
+
+        </Container>
     )
 }
 
