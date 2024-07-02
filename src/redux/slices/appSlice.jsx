35595424ed496e4b99fbabb60a6daebe296f996b
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-02 15:48:38 +0400

    developed

--- src/redux/slices/appSlice.jsx
+++ src/redux/slices/appSlice.jsx
@@ -3,3 +3,14 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     loading: false
 }
+
+export const appSlice = createSlice({
+    name: "app",
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+
+    }
+})
