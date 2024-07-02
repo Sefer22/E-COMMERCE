442df7450f2ba771ddd2db3f81bd73cf7a767daa
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-02 15:44:03 +0400

    developed

--- src/redux/slices/productSlice.jsx
+++ src/redux/slices/productSlice.jsx
@@ -1,1 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+const initalState = {
+    product: [],
+    selectedProduct: {}
+}
+
+export const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: {
+
+    }
+})
