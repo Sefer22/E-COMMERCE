e63c5e3095d2e6ad4f99d6977d48ad400e64d603
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-02 16:05:37 +0400

    developed

--- src/redux/slices/productSlice.jsx
+++ src/redux/slices/productSlice.jsx
@@ -11,6 +11,7 @@ const BASE_URL = "https://fakestoreapi.com"
 
 export const getAllProducts = createAsyncThunk("getAllProducts", async () => {
     const response = await axios.get(`${BASE_URL}/products`);
+    return response.data;
 })
 
 export const productSlice = createSlice({
@@ -20,7 +21,12 @@ export const productSlice = createSlice({
 
     },
     extraReducers: (builder) => {
+        builder.addCase(getAllProducts.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(getAllProducts.fulfilled, (state, action) => {
 
+        })
     }
 })
 
