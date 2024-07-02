a7c82370aec72efef1a2bdee708f7f7f0af5f94d
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-02 16:11:39 +0400

    developed

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { getAllProducts } from '../redux/slices/productSlice';
 
 function ProductList() {
-    useEffect(() => {
 
+    const dispatch = useDispatch();
+    const products = useSelector((store) => store.product);
+
+    useEffect(() => {
+        dispatch(getAllProducts());
     }, [])
 
     return (
