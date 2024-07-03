1d87fac849a4f92bb8e67c3f77a56a0a44319fd3
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-03 15:49:11 +0400

    developed

---
+++ src/config/RouterConfig.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+function RouterConfig() {
+    return (
+        <div>RouterConfig</div>
+    )
+}
+
+export default RouterConfig
