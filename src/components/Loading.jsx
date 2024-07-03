b2a7e9bc053e42e08a007fa407e8f6a7091be017
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-03 17:29:23 +0400

    developed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useSelector } from 'react-redux';
 
 function Loading() {
-    const [open, setOpen] = useState(false);
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleOpen = () => {
-        setOpen(true);
-    };
+
+    const { loading } = useSelector((store) => store.product);
 
     return (
-        <div>Loading</div>
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={loading}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
     )
 }
 
