c7aa2dd8348f7191ec815ab6aa9b7cfabf8e5dce
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-03 16:16:38 +0400

    developed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Product({ product }) {
+    const { id, price, image, title, description } = product;
     console.log(product)
     return (
         <div>Product</div>
