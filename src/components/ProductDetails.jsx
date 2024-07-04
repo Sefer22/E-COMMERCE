da9aaf47c37d9fdc4a2a08980bcb2d1319959574
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-04 18:05:07 +0400

    developed

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
 
 function ProductDetails() {
     const { id } = useParams();
+    const { product } = useSelector((store) => store.product);
+
+    useEffect(() => {
+
+    }, [])
     return (
         <div>ProductDetails {id}</div>
     )
