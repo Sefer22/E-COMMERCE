171748b97f5028bc3510195b705e8bafe6dc03a7
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-06 14:15:56 +0400

    developed

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import Header from './components/Header'
 import RouterConfig from './config/RouterConfig'
 import Loading from './components/Loading'
 import Drawer from '@mui/material/Drawer';
+import { useSelector } from 'react-redux'
 
 
 function App() {
+
+  const { products } = useSelector((store) => store.basket);
 
   return (
     <div>
@@ -15,7 +18,9 @@ function App() {
         <Loading />
         <Drawer className='drawer' anchor='right' open={true}>
           {
+            products && products.map((product) => {
 
+            })
           }
         </Drawer>
         <Header />
