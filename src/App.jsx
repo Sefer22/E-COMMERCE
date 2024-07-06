b44eae3b989dfda3169214b8a3536cdce304020f
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-07-06 14:27:18 +0400

    developed

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,11 @@ function App() {
           {
             products && products.map((product) => {
               return (
-                <div>
-                  <img src={product.image} width={50} height={50} />
-                  <p>{product.title}</p>
+                <div className='flex-row' style={{ padding: '20px' }}>
+                  <img style={{ marginRight: '5px' }} src={product.image} width={50} height={50} />
+                  <p style={{ width: '350px', marginRight: '5px' }}>{product.title}({product.count})</p>
+                  <p style={{ fontWeight: 'bold', marginRight: '10px' }}>{product.price} AZN</p>
+                  <button style={{ padding: '5px', borderRadius: '5px', backgroundColor: 'red', border: 'none', color: '#fff', width: '100px' }}>Delete</button>
                 </div>
               )
             })
